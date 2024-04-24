eb2bef0409fada8cf82974b7466e8324edebe012
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-04-23 22:54:53 -0400

    Update tacrp_eo_sg510.lua

--- lua/weapons/tacrp_eo_sg510.lua
+++ lua/weapons/tacrp_eo_sg510.lua
@@ -8,7 +8,7 @@ SWEP.PrintName = "SIG SG 510"
 SWEP.AbbrevName = "SG 510"
 SWEP.Category = "Tactical RP"
 
-SWEP.SubCatTier = "1Elite"
+SWEP.SubCatTier = "3Security"
 SWEP.SubCatType = "6Precision Rifle"
 
 SWEP.Description = "Vintage battle rifle with excellent long range performance. Recoil is harsh but stable."
