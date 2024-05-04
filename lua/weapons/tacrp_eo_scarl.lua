6c02b146c06cc1dd5bae9da629a6bd2cd60be3db
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-05-04 12:15:18 -0400

    scar l

---
+++ lua/weapons/tacrp_eo_scarl.lua
@@ -0,0 +1,311 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "FN SCAR-L"
+SWEP.AbbrevName = "SCAR-L"
+SWEP.Category = "Tactical RP"
+
+SWEP.SubCatTier = "2Operator"
+SWEP.SubCatType = "4Assault Rifle"
+
+SWEP.Description = "Lightweight assault rifle."
+
+SWEP.Trivia_Caliber = "5.56x45mm"
+SWEP.Trivia_Manufacturer = "FN Herstal"
+SWEP.Trivia_Year = "2004"
+
+SWEP.Faction = TacRP.FACTION_MILITIA
+SWEP.Credits = "Assets: Counter-Strike: Online 2"
+
+SWEP.ViewModel = "models/weapons/tacint_shark/v_scarl.mdl"
+SWEP.WorldModel = "models/weapons/tacint_shark/w_scarl.mdl"
+
+SWEP.Slot = 2
+
+SWEP.BalanceStats = {
+    [TacRP.BALANCE_SBOX] = {
+        Damage_Max = 23,
+        Damage_Min = 10,
+
+        RecoilKick = 1.5,
+    },
+    [TacRP.BALANCE_TTT] = {
+        Damage_Max = 15,
+        Damage_Min = 10,
+        Range_Min = 600,
+        Range_Max = 2000,
+        PostBurstDelay = 0.25,
+
+        RecoilSpreadPenalty = 0.0025,
+
+        BodyDamageMultipliers = {
+            [HITGROUP_HEAD] = 3,
+            [HITGROUP_CHEST] = 1.25,
+            [HITGROUP_STOMACH] = 1,
+            [HITGROUP_LEFTARM] = 0.9,
+            [HITGROUP_RIGHTARM] = 0.9,
+            [HITGROUP_LEFTLEG] = 0.75,
+            [HITGROUP_RIGHTLEG] = 0.75,
+            [HITGROUP_GEAR] = 0.9
+        },
+    },
+    [TacRP.BALANCE_PVE] = {
+        Damage_Max = 9,
+        Damage_Min = 4,
+    },
+    [TacRP.BALANCE_OLDSCHOOL] = {
+        HipFireSpreadPenalty = 0.009,
+        RecoilSpreadPenalty = 0.003
+    }
+}
+
+SWEP.TTTReplace = TacRP.TTTReplacePreset.AssaultRifle
+
+// "ballistics"
+
+SWEP.Damage_Max = 18
+SWEP.Damage_Min = 12
+SWEP.Range_Min = 1000 // distance for which to maintain maximum damage
+SWEP.Range_Max = 3200 // distance at which we drop to minimum damage
+SWEP.Penetration = 7 // units of metal this weapon can penetrate
+SWEP.ArmorPenetration = 0.75
+
+SWEP.BodyDamageMultipliers = {
+    [HITGROUP_HEAD] = 2,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_STOMACH] = 1.25,
+    [HITGROUP_LEFTARM] = 1,
+    [HITGROUP_RIGHTARM] = 1,
+    [HITGROUP_LEFTLEG] = 0.85,
+    [HITGROUP_RIGHTLEG] = 0.85,
+    [HITGROUP_GEAR] = 0.9
+}
+
+SWEP.MuzzleVelocity = 25000
+
+// misc. shooting
+
+SWEP.Firemodes = {2, 1}
+SWEP.PostBurstDelay = 0.16
+
+SWEP.RPM = 650
+
+SWEP.Spread = 0.004
+SWEP.RecoilSpreadPenalty = 0.002
+SWEP.HipFireSpreadPenalty = 0.02
+
+SWEP.RecoilResetInstant = false
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 10
+SWEP.RecoilResetTime = 0.06
+SWEP.RecoilDissipationRate = 35
+SWEP.RecoilFirstShotMult = 2
+
+SWEP.RecoilVisualKick = 0.75
+SWEP.RecoilKick = 2.5
+SWEP.RecoilStability = 0.3
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.925
+SWEP.ShootingSpeedMult = 0.8
+SWEP.SightedSpeedMult = 0.65
+
+SWEP.ReloadSpeedMult = 0.5
+
+SWEP.AimDownSightsTime = 0.35
+SWEP.SprintToFireTime = 0.375
+
+SWEP.Sway = 1.25
+SWEP.ScopedSway = 0.15
+SWEP.FreeAimMaxAngle = 3.5
+
+// hold types
+
+SWEP.HoldType = "ar2"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -6)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(3, -2, -5)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+SWEP.SightAng = Angle(0, 0.35, 0)
+SWEP.SightPos = Vector(-4.42, -7.5, -5.5)
+
+SWEP.CorrectivePos = Vector(0, 0, 0.1)
+SWEP.CorrectiveAng = Angle(0, 0, 0)
+
+SWEP.HolsterVisible = true
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_BACK
+SWEP.HolsterPos = Vector(5, 0, -6)
+SWEP.HolsterAng = Angle(0, 0, 0)
+
+// reload
+
+SWEP.ClipSize = 30
+SWEP.Ammo = "smg1"
+
+SWEP.ReloadTimeMult = 1
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/m4.mdl"
+SWEP.DropMagazineImpact = "plastic"
+
+SWEP.ReloadUpInTime = 1.3
+SWEP.DropMagazineTime = 0.4
+
+// sounds
+
+local path = "tacrp/weapons/k1a/"
+local path1 = "tacint_shark/weapons/scarl/scarl"
+
+SWEP.Sound_Shoot = "^" .. path1 .. "_unsil-1.wav"
+SWEP.Sound_Shoot_Silenced = path1 .. "-1.wav"
+
+SWEP.Vol_Shoot = 110
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_ak47"
+SWEP.EjectEffect = 2
+
+// anims
+
+SWEP.AnimationTranslationTable = {
+    ["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"}
+}
+
+SWEP.ProceduralIronFire = {
+    vm_pos = Vector(0, -0.5, -0.125),
+    vm_ang = Angle(0, 0.5, 0),
+    t = 0.15,
+    tmax = 0.2,
+    bones = {
+        {
+            bone = "ValveBiped.bolt_cover",
+            pos = Vector(0, 0, -3),
+            t0 = 0,
+            t1 = 0.1,
+        },
+    },
+}
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["sights"] = {
+        BGs_VM = {
+            {1, 1}
+        },
+    },
+}
+
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Optic",
+        Category = {"optic_cqb", "optic_medium"}, //, "optic_sniper"
+        Bone = "ValveBiped._ROOT_K1a",
+        WMBone = "Box01",
+		InstalledElements = {"sights"},
+        AttachSound = "tacrp/weapons/optic_on.wav",
+        DetachSound = "tacrp/weapons/optic_off.wav",
+        VMScale = 1.1,
+        Pos_VM = Vector(-5.9, 0.25, 6),
+        Pos_WM = Vector(0, 5, 1.4),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [2] = {
+        PrintName = "Muzzle",
+        Category = "silencer",
+        Bone = "ValveBiped._ROOT_K1a",
+        WMBone = "Box01",
+        AttachSound = "tacrp/weapons/silencer_on.wav",
+        DetachSound = "tacrp/weapons/silencer_off.wav",
+        Pos_VM = Vector(-3.22, 0.38, 27.5),
+        Pos_WM = Vector(0, 26, -1.1),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [3] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "ValveBiped._ROOT_K1a",
+        WMBone = "Box01",
+		VMScale = 1.1,
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+        Pos_VM = Vector(-3.3, -0.9, 15),
+        Pos_WM = Vector(1.2, 15, -1),
+        Ang_VM = Angle(90, 0, -90),
+        Ang_WM = Angle(-90, -90, 0),
+    },
+    [4] = {
+        PrintName = "Accessory",
+        Category = {"acc", "acc_sling", "acc_duffle", "perk_extendedmag"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+    [5] = {
+        PrintName = "Bolt",
+        Category = {"bolt_automatic"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [6] = {
+        PrintName = "Trigger",
+        Category = {"trigger_auto"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [7] = {
+        PrintName = "Ammo",
+        Category = {"ammo_rifle"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [8] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_scarl.Remove_Clip", path1 .. "_clipout.wav")
+addsound("tacint_scarl.Insert_Clip", path1 .. "_clipin.wav")
+addsound("tacint_scarl.bolt_back", path1 .. "_boltpull.wav")
+addsound("tacint_scarl.bolt_forward", path .. "bolt_forward-1.wav")
