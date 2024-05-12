aa21a0f258440d603860ab08b374385942282ab1
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-05-12 18:40:11 -0400

    auga one

---
+++ lua/weapons/tacrp_eo_auga1.lua
@@ -0,0 +1,314 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "Steyr AUG A1"
+SWEP.AbbrevName = "AUG A1"
+SWEP.Category = "Tactical RP"
+
+SWEP.SubCatTier = "3Security"
+SWEP.SubCatType = "4Assault Rifle"
+
+SWEP.Description = "Classic AUG variant with a larger magazine and full-auto. \nComes with a 1.5x optic that cannot be removed or adjusted."
+SWEP.Description_Quote = ""
+
+SWEP.Trivia_Caliber = "5.56x45mm"
+SWEP.Trivia_Manufacturer = "Steyr Arms"
+SWEP.Trivia_Year = "1977"
+
+SWEP.Faction = TacRP.FACTION_COALITION
+SWEP.Credits = [[
+Model: Twinke Masta
+Textures: Silentassassin12
+Sound & Animations: Tactical Intervention 
+]]
+
+SWEP.ViewModel = "models/weapons/tacint_shark/v_auga1.mdl"
+SWEP.WorldModel = "models/weapons/tacint_shark/w_auga1.mdl"
+
+SWEP.Slot = 2
+
+SWEP.BalanceStats = {
+    [TacRP.BALANCE_SBOX] = {
+        Damage_Max = 20,
+        Damage_Min = 12,
+    },
+    [TacRP.BALANCE_TTT] = {
+        Damage_Max = 14,
+        Damage_Min = 10,
+        Range_Min = 400,
+        Range_Max = 1800,
+        PostBurstDelay = 0.2,
+
+        RecoilResetInstant = true,
+        RecoilResetTime = 0.2,
+        RecoilSpreadPenalty = 0.004,
+        RecoilDissipationRate = 18,
+
+        BodyDamageMultipliers = {
+            [HITGROUP_HEAD] = 3,
+            [HITGROUP_CHEST] = 1.25,
+            [HITGROUP_STOMACH] = 1,
+            [HITGROUP_LEFTARM] = 0.9,
+            [HITGROUP_RIGHTARM] = 0.9,
+            [HITGROUP_LEFTLEG] = 0.75,
+            [HITGROUP_RIGHTLEG] = 0.75,
+            [HITGROUP_GEAR] = 0.9
+        },
+    },
+    [TacRP.BALANCE_PVE] = {
+        Damage_Max = 11,
+        Damage_Min = 7,
+    },
+    [TacRP.BALANCE_OLDSCHOOL] = {
+        RecoilDissipationRate = 15,
+        RecoilMaximum = 15,
+        RecoilSpreadPenalty = 0.005,
+        HipFireSpreadPenalty = 0.007,
+    }
+}
+
+SWEP.TTTReplace = TacRP.TTTReplacePreset.AssaultRifle
+
+// "ballistics"
+
+SWEP.Damage_Max = 20
+SWEP.Damage_Min = 12
+SWEP.Range_Min = 1100
+SWEP.Range_Max = 3300
+SWEP.Penetration = 7
+SWEP.ArmorPenetration = 0.8
+
+SWEP.BodyDamageMultipliers = {
+    [HITGROUP_HEAD] = 5,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_STOMACH] = 1.25,
+    [HITGROUP_LEFTARM] = 1,
+    [HITGROUP_RIGHTARM] = 1,
+    [HITGROUP_LEFTLEG] = 0.9,
+    [HITGROUP_RIGHTLEG] = 0.9,
+    [HITGROUP_GEAR] = 0.9
+}
+
+SWEP.MuzzleVelocity = 24000
+
+// misc. shooting
+
+SWEP.Firemodes = {
+    2,
+    1
+}
+
+SWEP.RPM = 700
+SWEP.RPMMultBurst = 900 / 700
+
+SWEP.Spread = 0.0035
+
+SWEP.PostBurstDelay = 0.12
+
+SWEP.RunawayBurst = true
+
+SWEP.RecoilResetInstant = false
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 12
+SWEP.RecoilResetTime = 0.04
+SWEP.RecoilDissipationRate = 36
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 0.75
+
+SWEP.RecoilKick = 3
+SWEP.RecoilStability = 0.25
+
+SWEP.RecoilSpreadPenalty = 0.002
+SWEP.HipFireSpreadPenalty = 0.04
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.9
+SWEP.ShootingSpeedMult = 0.7
+SWEP.SightedSpeedMult = 0.7
+
+SWEP.ReloadSpeedMult = 0.5
+
+SWEP.AimDownSightsTime = 0.35
+SWEP.SprintToFireTime = 0.3
+
+SWEP.Sway = 1
+SWEP.ScopedSway = 0.125
+
+SWEP.FreeAimMaxAngle = 2
+
+// hold types
+
+SWEP.HoldType = "smg"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -5.5)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(3, -2, -5)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+SWEP.SightAng = Angle(0.01, 0.12, 0)
+SWEP.SightPos = Vector(-3.93, -6, -4.9)
+
+SWEP.CorrectivePos = Vector(0.12, 0, 0)
+SWEP.CorrectiveAng = Angle(0, 0, -1)
+
+SWEP.HolsterVisible = true
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_BACK
+SWEP.HolsterPos = Vector(5, 4, -6)
+SWEP.HolsterAng = Angle(0, 0, 0)
+
+// scope
+
+SWEP.Scope = true
+SWEP.ScopeOverlay = Material("tacrp/scopes/swarovski.png", "mips smooth") // Material("path/to/overlay")
+SWEP.ScopeFOV = 90 / 1.5
+SWEP.ScopeLevels = 1 // 2 = like CS:S
+SWEP.ScopeHideWeapon = true
+
+// reload
+
+SWEP.ClipSize = 42
+SWEP.Ammo = "smg1"
+
+SWEP.ReloadTimeMult = 1.08
+SWEP.DropMagazineModel = "models/weapons/tacint_shark/magazines/auga1.mdl"
+SWEP.DropMagazineImpact = "plastic"
+
+SWEP.ReloadUpInTime = 1.7
+SWEP.DropMagazineTime = 0.8
+
+// sounds
+
+local path = "tacrp/weapons/aug/aug_"
+
+SWEP.Sound_Shoot = "^" .. path .. "fire-1.wav"
+SWEP.Sound_Shoot_Silenced = path .. "fire_silenced-1.wav"
+
+SWEP.Vol_Shoot = 115
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_5"
+SWEP.EjectEffect = 2
+
+// anims
+
+SWEP.AnimationTranslationTable = {
+    ["fire_iron"] = "dryfire",
+    ["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"}
+}
+
+SWEP.ProceduralIronFire = {
+    vm_pos = Vector(0, -0.2, -0.04),
+    vm_ang = Angle(0, 0.25, 0),
+    t = 0.1,
+    tmax = 0.1,
+}
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["irons"] = {
+        BGs_VM = {
+            {1, 1}
+        },
+    },
+}
+
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Muzzle",
+        Category = "silencer",
+        Bone = "ValveBiped.AUG_rootbone",
+        AttachSound = "tacrp/weapons/silencer_on.wav",
+        DetachSound = "tacrp/weapons/silencer_off.wav",
+        Pos_VM = Vector(-3.9, -0.08, 22.5),
+        Ang_VM = Angle(90, 0, 0),
+        Pos_WM = Vector(26, 1, -4.2),
+        Ang_WM = Angle(0, 0, 180),
+    },
+    [2] = {
+        PrintName = "Tactical",
+        Category = {"tactical", "tactical_zoom", "tactical_ebullet"},
+        Bone = "ValveBiped.AUG_rootbone",
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+        InstalledElements = {"tactical"},
+        Pos_VM = Vector(-4, 0.8, 4),
+        Ang_VM = Angle(90, 0, 45),
+        Pos_WM = Vector(9, 0.2, -4),
+        Ang_WM = Angle(0, 0, -135),
+    },
+    [3] = {
+        PrintName = "Accessory",
+        Category = {"acc", "acc_sling", "acc_duffle", "perk_extendedmag"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+    [4] = {
+        PrintName = "Bolt",
+        Category = {"bolt_automatic"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [5] = {
+        PrintName = "Trigger",
+        Category = {"trigger_burst"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [6] = {
+        PrintName = "Ammo",
+        Category = {"ammo_rifle"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [7] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_auga1.insert_clip", path .. "insert_clip.wav")
+addsound("tacint_auga1.remove_clip", path .. "remove_clip.wav")
+addsound("tacint_auga1.Handle_FoldDown", path .. "handle_folddown.wav")
+addsound("tacint_auga1.bolt_lockback", path .. "bolt_lockback.wav")
+addsound("tacint_auga1.bolt_release", path .. "bolt_release.wav")
