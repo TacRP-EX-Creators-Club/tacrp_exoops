7cc1d702e74cdd3d06a623d0a70ba79430aaca90
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-10-14 14:31:45 -0400

    Update tacrp_eo_hushpup.lua

--- lua/weapons/tacrp_eo_hushpup.lua
+++ lua/weapons/tacrp_eo_hushpup.lua
@@ -263,7 +263,7 @@ SWEP.Attachments = {
     },
     [2] = {
         PrintName = "Tactical",
-        Category = {"tactical", "tactical_zoom", "tactical_ebullet"},
+        Category = {"tactical", "tactical_ebullet"},
         Bone = "ValveBiped.GSR1911_rootbone",
         WMBone = "Box01",
         AttachSound = "TacRP/weapons/flashlight_on.wav",
